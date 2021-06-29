5c72c4a8efed889a127d0cf354eac1583e17c750
David Brungardt <brungardtdb@gmail.com>
2021-06-29 13:13:51 -0600

    Implemented Repositories
    Implemented repositories for Leave Type, Allocation, and History.

--- leave-management/Repository/LeaveAllocationRepository.cs
+++ leave-management/Repository/LeaveAllocationRepository.cs
@@ -18,32 +18,38 @@ namespace leave_management.Repository
 
         public bool Create(LeaveAllocation entity)
         {
-            throw new NotImplementedException();
+            _db.LeaveAllocations.Add(entity);
+            return Save();
         }
 
         public bool Delete(LeaveAllocation entity)
         {
-            throw new NotImplementedException();
+            _db.LeaveAllocations.Remove(entity);
+            return Save();
         }
 
         public ICollection<LeaveAllocation> FindAll()
         {
-            throw new NotImplementedException();
+            var leaveAllocations = _db.LeaveAllocations.ToList();
+            return leaveAllocations;
         }
 
         public LeaveAllocation FindById(int id)
         {
-            throw new NotImplementedException();
+            var leaveAllocations = _db.LeaveAllocations.Find(id);
+            return leaveAllocations;
         }
 
         public bool Save()
         {
-            throw new NotImplementedException();
+            int changes = _db.SaveChanges();
+            return changes > 0;
         }
 
         public bool Update(LeaveAllocation entity)
         {
-            throw new NotImplementedException();
+            _db.LeaveAllocations.Update(entity);
+            return Save();
         }
     }
 }
--- leave-management/Repository/LeaveHistoryRepository.cs
+++ leave-management/Repository/LeaveHistoryRepository.cs
@@ -18,32 +18,38 @@ namespace leave_management.Repository
 
         public bool Create(LeaveHistory entity)
         {
-            throw new NotImplementedException();
+            _db.LeaveHistories.Add(entity);
+            return Save();
         }
 
         public bool Delete(LeaveHistory entity)
         {
-            throw new NotImplementedException();
+            _db.LeaveHistories.Remove(entity);
+            return Save();
         }
 
         public ICollection<LeaveHistory> FindAll()
         {
-            throw new NotImplementedException();
+            var leaveHistories = _db.LeaveHistories.ToList();
+            return leaveHistories;
         }
 
         public LeaveHistory FindById(int id)
         {
-            throw new NotImplementedException();
+            var leaveHistory = _db.LeaveHistories.Find(id);
+            return leaveHistory;
         }
 
         public bool Save()
         {
-            throw new NotImplementedException();
+            var updates = _db.SaveChanges();
+            return updates > 0;
         }
 
         public bool Update(LeaveHistory entity)
         {
-            throw new NotImplementedException();
+            _db.LeaveHistories.Update(entity);
+            return Save();
         }
     }
 }
