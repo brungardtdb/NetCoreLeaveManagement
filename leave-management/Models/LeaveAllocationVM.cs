c85c22238484fc554ddab2700688fba8656be69b
David Brungardt <brungardtdb@gmail.com>
2021-06-29 13:54:52 -0600

    Created VM for Leave Allocation

---
+++ leave-management/Models/LeaveAllocationVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Models
+{
+    public class LeaveAllocationVM
+    {
+        public int Id { get; set; }
+        public int NumberOfDays { get; set; }
+        public DateTime DateCreated { get; set; }
+        public EmployeeVM Employee { get; set; }
+        public string EmployeeId { get; set; }
+        public DetailsLeaveTypeVM LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+    }
+}
